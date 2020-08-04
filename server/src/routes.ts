916f42dd0a2f4c4e73dde2e9aca9e58a62d0d4a1
Felipe Suri <felipeheredia1234@gmail.com>
2020-08-04 15:53:10 -0300

    :truck: create route for create class

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,61 @@
 import { Router } from 'express'
 
+import database from './database/DBConnection'
+import convertHourToMinutes from './utils/convertHoursToMinutes'
+
 const routes = Router()
+
+interface ScheduleItem {
+  week_day: number
+  from: string
+  to: string
+}
+
+routes.post('/classes', async (request, response) => {
+  const { name, avatar, whatsapp, bio, subject, cost, schedule } = request.body
+
+  const trx = await database.transaction()
+
+  try {
+    const insertedUsersIds = await trx('users').insert({
+      name,
+      avatar,
+      whatsapp,
+      bio,
+    })
+
+    const user_id = insertedUsersIds[0]
+
+    const insertedClassesIds = await trx('classes').insert({
+      subject,
+      cost,
+      user_id,
+    })
+
+    const class_id = insertedClassesIds[0]
+
+    const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+      return {
+        class_id,
+        week_day: scheduleItem.week_day,
+        from: convertHourToMinutes(scheduleItem.from),
+        to: convertHourToMinutes(scheduleItem.to),
+      }
+    })
+
+    await trx('class_schedule').insert(classSchedule)
+
+    await trx.commit()
+
+    return response.status(201).send()
+  } catch (err) {
+    await trx.rollback()
+
+    return response.status(400).json({
+      status: 'error',
+      message: 'Unexpected error while creating new class',
+    })
+  }
+})
 
 export default routes
