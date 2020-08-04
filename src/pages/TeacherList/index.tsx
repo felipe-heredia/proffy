13f3e63dad736ca1616aa088570a22a0b00a7c73
Felipe Suri <felipeheredia1234@gmail.com>
2020-08-04 10:59:55 -0300

    :lipstick: finish TeacherList

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,5 +1,37 @@
 import React from 'react'
 
-const TeacherList: React.FC = () => <h1>Teachers</h1>
+import PageHeader from '../../components/PageHeader'
+import TeacherItem from '../../components/TeacherItem'
+
+import * as S from './styled'
+
+const TeacherList: React.FC = () => (
+  <S.TeacherListWrapper>
+    <PageHeader title="Esses são os proffys disponíveis">
+      <S.SearchTeachers>
+        <S.InputBlock>
+          <label htmlFor="subject">Matéria</label>
+          <input type="text" id="subject" />
+        </S.InputBlock>
+        <S.InputBlock>
+          <label htmlFor="week-day">Dia da semana</label>
+          <input type="text" id="week-day" />
+        </S.InputBlock>
+        <S.InputBlock>
+          <label htmlFor="time">hora</label>
+          <input type="text" id="time" />
+        </S.InputBlock>
+      </S.SearchTeachers>
+    </PageHeader>
+
+    <S.ListAllTeachers>
+      <TeacherItem />
+      <TeacherItem />
+      <TeacherItem />
+      <TeacherItem />
+      <TeacherItem />
+    </S.ListAllTeachers>
+  </S.TeacherListWrapper>
+)
 
 export default TeacherList
