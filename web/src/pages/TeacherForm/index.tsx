20de3812e749aa559d7d0eae74c2c84c0423e39f
Felipe Suri <felipeheredia1234@gmail.com>
2020-08-05 17:00:29 -0300

    :zap: finish TeacherForm page

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,13 +1,108 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import PageHeader from '../../components/PageHeader'
+import Input from '../../components/Input'
+
+import warningIcon from '../../images/icons/warning.svg'
 
 import * as S from './styled'
+import TextArea from '../../components/TextArea'
+import Select from '../../components/Select'
 
-const TeacherForm: React.FC = () => (
-  <S.PageTeacherForm>
-    <PageHeader title="Incrível que você quer dar aulas" />
-  </S.PageTeacherForm>
-)
+const TeacherForm: React.FC = () => {
+  const [scheduleItems, setScheduleItems] = useState([{ week_day: 0, from: '', to: '' }])
+
+  const labelColor = 'var(--color-text-complement)'
+
+  function addNewScheduleItem() {
+    setScheduleItems([...scheduleItems, { week_day: 0, from: '8:00 AM', to: '4:00 PM' }])
+  }
+
+  return (
+    <S.PageTeacherForm>
+      <PageHeader
+        title="Incrível que você quer dar aulas"
+        description="O primeiro passo é preencher este formulário de inscrição"
+      />
+
+      <S.FormWrapper>
+        <S.Fieldset>
+          <legend>Seus dados</legend>
+
+          <Input name="name" label="Nome completo" labelcolor={labelColor} />
+          <Input name="avatar" label="Avatar" labelcolor={labelColor} />
+          <Input name="whatsapp" label="Whatsapp" labelcolor={labelColor} />
+        </S.Fieldset>
+
+        <S.Fieldset>
+          <legend>Sobre a aula</legend>
+
+          <Select
+            name="subject"
+            label="Matéria"
+            labelcolor={labelColor}
+            options={[
+              { value: 'Artes', label: 'Artes' },
+              { value: 'Biologia', label: 'Biologia' },
+              { value: 'Educação Física', label: 'Educação Física' },
+              { value: 'Filosofia', label: 'Filosofia' },
+              { value: 'Física', label: 'Física' },
+              { value: 'Geografia', label: 'Geografia' },
+              { value: 'Inglês', label: 'Inglês' },
+              { value: 'História', label: 'História' },
+              { value: 'Matemática', label: 'matemática' },
+              { value: 'Português', label: 'Português' },
+              { value: 'Química', label: 'Química' },
+              { value: 'Sociologia', label: 'Sociologia' },
+            ]}
+          />
+          <Input name="cost" label="Custo da hora por aula" labelcolor={labelColor} />
+          <TextArea name="bio" label="Biografia" labelcolor={labelColor} />
+        </S.Fieldset>
+
+        <S.Fieldset>
+          <legend>
+            Horários disponíveis
+            <S.TimeButton type="button" onClick={addNewScheduleItem}>
+              + Novo horário
+            </S.TimeButton>
+          </legend>
+
+          {scheduleItems.map(scheduleItem => {
+            return (
+              <S.ScheduleItem key={scheduleItem.week_day}>
+                <Select
+                  name="week_day"
+                  label="Dia da semana"
+                  labelcolor={labelColor}
+                  options={[
+                    { value: '0', label: 'Domingo' },
+                    { value: '1', label: 'Segunda-feira' },
+                    { value: '2', label: 'Terça-feira' },
+                    { value: '3', label: 'Quarta-feira' },
+                    { value: '4', label: 'Quinta-feira' },
+                    { value: '5', label: 'Sexta-feira' },
+                    { value: '6', label: 'Sábado' },
+                  ]}
+                />
+                <Input name="from" label="Das" type="time" labelcolor={labelColor} />
+                <Input name="to" label="Até" type="time" labelcolor={labelColor} />
+              </S.ScheduleItem>
+            )
+          })}
+        </S.Fieldset>
+
+        <S.FormFooter>
+          <p>
+            <img src={warningIcon} alt="Aviso importante" />
+            Importante <br />
+            Preencha todos os dados
+          </p>
+          <S.FooterButton type="button">Salvar cadastro</S.FooterButton>
+        </S.FormFooter>
+      </S.FormWrapper>
+    </S.PageTeacherForm>
+  )
+}
 
 export default TeacherForm
--- web/src/pages/TeacherForm/styled.ts
+++ web/src/pages/TeacherForm/styled.ts
@@ -1,3 +1,129 @@
 import styled from 'styled-components'
+import media from 'styled-media-query'
 
-export const PageTeacherForm = styled.div``
+export const PageTeacherForm = styled.div`
+  width: 100vw;
+  height: 100vh;
+  max-width: 700px;
+
+  ${media.greaterThan('medium')`
+    max-width: 100vw;
+  `}
+`
+
+export const FormWrapper = styled.main`
+  background: var(--color-box-base);
+  width: 100%;
+  max-width: 75rem;
+  border-radius: 0.8rem;
+  margin: 0 auto 3.2rem;
+  padding-top: 6.4rem;
+  overflow: hidden;
+
+  ${media.greaterThan('medium')`
+    margin-top: -2rem;
+  `}
+`
+
+export const Fieldset = styled.fieldset`
+  border: 0;
+  padding: 0 2.4rem;
+
+  & + & {
+    margin-top: 6.4rem;
+  }
+
+  ${media.greaterThan('medium')`
+    padding: 0 6.4rem;
+  `}
+
+  legend {
+    font: 700 2.4rem Archivo;
+    color: var(--color-text-title);
+    margin-bottom: 2.4rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    padding-bottom: 1.6rem;
+    border-bottom: 1px solid var(--color-line-in-white);
+  }
+`
+
+export const ScheduleItem = styled.div`
+  ${media.greaterThan('medium')`
+    display: grid;
+    grid-template-columns: 2fr 1fr 1fr;
+    column-gap: 1.6rem;
+  `}
+`
+
+export const TimeButton = styled.button`
+  background: none;
+  border: 0;
+  color: var(--color-primary);
+  font: 700 1.6rem Archivo;
+  cursor: pointer;
+  transition: color 0.2s;
+
+  &:hover {
+    color: var(--color-primary-dark);
+  }
+`
+
+export const FormFooter = styled.footer`
+  padding: 4rem 2.4rem;
+  background: var(--color-box-footer);
+  border-top: 1px solid var(--color-line-in-white);
+  margin-top: 6.4rem;
+
+  ${media.greaterThan('medium')`
+    padding: 4.0rem 6.4rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  `}
+
+  p {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.4rem;
+    line-height: 2.4rem;
+    color: var(--color-text-complement);
+
+    ${media.greaterThan('medium')`
+      justify-content: space-between;
+    `}
+
+    img {
+      margin-right: 2rem;
+    }
+  }
+`
+
+export const FooterButton = styled.button`
+  width: 100%;
+  height: 5.6rem;
+  background: var(--color-secondary);
+  color: var(--color-button-text);
+  border: 0;
+  border-radius: 0.8rem;
+  cursor: pointer;
+  font: 700 1.6rem Archivo;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-decoration: none;
+  transition: background-color 0.2s;
+  margin-top: 2.3rem;
+
+  ${media.greaterThan('medium')`
+    width: 20rem;
+    margin-top: 0;
+  `}
+
+  &:hover {
+    background: var(--color-secondary-dark);
+  }
+`
