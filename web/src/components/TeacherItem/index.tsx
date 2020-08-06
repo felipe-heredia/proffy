fd72d61a0b5a4ae5d83c19f69c99120a7b4dd8af
Felipe Suri <felipeheredia1234@gmail.com>
2020-08-06 10:45:55 -0300

    :alien: connect to api

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,39 +3,57 @@ import React from 'react'
 import whatsappIcon from '../../images/icons/whatsapp.svg'
 
 import * as S from './styled'
+import { api } from '../../services/api'
 
-const TeacherItem: React.FC = () => (
-  <S.TeacherItem>
-    <header>
-      <img
-        src="https://suri-gobarber.s3.us-east-2.amazonaws.com/e4673dd731a77ee71b4a-50281523.jpeg"
-        alt="felipesuri"
-      />
-      <div>
-        <strong>felipesuri</strong>
-        <span>Português</span>
-      </div>
-    </header>
+export interface Teacher {
+  id: number
+  avatar: string
+  bio: string
+  cost: number
+  name: string
+  subject: string
+  whatsapp: string
+}
 
-    <p>
-      Gosta muito de escrever artigos para o seu blog sobre programação.
-      <br />
-      <br />
-      Apaixonado por escrita de código e também por escrita de artigos que mudarão a vida
-      das pessoas.
-    </p>
+interface TeacherItemProps {
+  teacher: Teacher
+}
 
-    <footer>
-      <p>
-        Preço/hora
-        <strong>R$ 110,00</strong>
-      </p>
-      <button type="button">
-        <img src={whatsappIcon} alt="Whatsapp" />
-        Entrar em contato
-      </button>
-    </footer>
-  </S.TeacherItem>
-)
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    })
+  }
+
+  return (
+    <S.TeacherItem>
+      <header>
+        <img src={teacher.avatar} alt={teacher.name} />
+        <div>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
+        </div>
+      </header>
+
+      <p>{teacher.bio}</p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ {teacher.cost}</strong>
+        </p>
+        <a
+          onClick={createNewConnection}
+          target="_blank"
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em contato
+        </a>
+      </footer>
+    </S.TeacherItem>
+  )
+}
 
 export default TeacherItem
