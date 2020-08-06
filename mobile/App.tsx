688e7658da1758df7ed60d7ac9f31ecdb78a478a
Felipe Suri <felipeheredia1234@gmail.com>
2020-08-06 14:37:02 -0300

    :alembic: use fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
+import { AppLoading } from 'expo'
+import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo'
+import { Poppins_600SemiBold, Poppins_400Regular } from '@expo-google-fonts/poppins'
 
 import Landing from './src/pages/Landing'
 
-const App: React.FC = () => (
-  <>
-    <Landing />
-    <StatusBar style="auto" />
-  </>
-)
+const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_600SemiBold,
+    Poppins_400Regular,
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
+  return (
+    <>
+      <Landing />
+      <StatusBar style="auto" />
+    </>
+  )
+}
 
 export default App
