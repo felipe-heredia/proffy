933a2a81cbe5c576329b5eba8323d7d60cff0c9f
Felipe Suri <felipeheredia1234@gmail.com>
2020-08-06 16:29:34 -0300

    :sparkles: create GiveClasses page

---
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+import * as S from './styled'
+
+const GiveClasses: React.FC = () => (
+  <S.GiveClassesWrapper>
+    <Text>Hello</Text>
+  </S.GiveClassesWrapper>
+)
+
+export default GiveClasses
---
+++ mobile/src/pages/GiveClasses/styled.ts
@@ -0,0 +1,3 @@
+import styled from 'styled-components/native'
+
+export const GiveClassesWrapper = styled.View``
