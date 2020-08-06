9a288f009c9edfada6df910bfb3d82d984aadefb
Felipe Suri <felipeheredia1234@gmail.com>
2020-08-06 10:48:04 -0300

    :lipstick: create one new grid column

--- web/src/pages/TeacherList/styled.ts
+++ web/src/pages/TeacherList/styled.ts
@@ -16,11 +16,37 @@ export const SearchTeachers = styled.form`
 
   ${media.greaterThan('medium')`
     display: grid;
-    grid-template-columns: repeat(3, 1fr);
+    grid-template-columns: repeat(4, 1fr);
     column-gap: 16px;
     position: absolute;
     bottom: -5.2rem;
   `}
+`
+
+export const SearchButton = styled.button`
+  width: 100%;
+  height: 5rem;
+  background: var(--color-secondary);
+  color: var(--color-button-text);
+  border: 0;
+  border-radius: 0.8rem;
+  cursor: pointer;
+  font: 700 1.6rem Archivo;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-decoration: none;
+  transition: background-color 0.2s;
+  margin-top: 2.3rem;
+
+  ${media.greaterThan('medium')`
+    width: 20rem;
+    margin-top: 2.8rem;
+  `}
+
+  &:hover {
+    background: var(--color-secondary-dark);
+  }
 `
 
 export const ListAllTeachers = styled.main`
