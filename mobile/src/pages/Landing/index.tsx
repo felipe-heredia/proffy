48fc253933bd677c952ff2bcc00ae34eca8d0416
Felipe Suri <felipeheredia1234@gmail.com>
2020-08-06 16:30:21 -0300

    :truck: use route for giveClasses

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-
+import { useNavigation } from '@react-navigation/native'
 import { Image } from 'react-native'
+
 import landingImg from '../../assets/images/landing.png'
 import studyIcon from '../../assets/images/icons/study.png'
 import giveClassesIcon from '../../assets/images/icons/give-classes.png'
@@ -8,32 +9,40 @@ import heartIcon from '../../assets/images/icons/heart.png'
 
 import * as S from './styled'
 
-const Landing: React.FC = () => (
-  <S.LandingPageWrapper>
-    <S.Banner style={{ resizeMode: 'contain' }} source={landingImg} />
+const Landing: React.FC = () => {
+  const navigation = useNavigation()
 
-    <S.Title>
-      Seja bem-vindo,
-      {'\n'}
-      <S.TitleBold>O que deseja fazer?</S.TitleBold>
-    </S.Title>
+  function handleNavigateToGiveClassesPage() {
+    navigation.navigate('GiveClasses')
+  }
 
-    <S.ButtonsContainer>
-      <S.ButtonPrimary>
-        <Image source={studyIcon} />
-        <S.ButtonText>Estudar</S.ButtonText>
-      </S.ButtonPrimary>
-      <S.ButtonSecondary>
-        <Image source={giveClassesIcon} />
-        <S.ButtonText>Estudar</S.ButtonText>
-      </S.ButtonSecondary>
-    </S.ButtonsContainer>
+  return (
+    <S.LandingPageWrapper>
+      <S.Banner style={{ resizeMode: 'contain' }} source={landingImg} />
 
-    <S.TotalConnections>
-      Total de 285 conexões já realizadas
-      <Image source={heartIcon} />
-    </S.TotalConnections>
-  </S.LandingPageWrapper>
-)
+      <S.Title>
+        Seja bem-vindo,
+        {'\n'}
+        <S.TitleBold>O que deseja fazer?</S.TitleBold>
+      </S.Title>
+
+      <S.ButtonsContainer>
+        <S.ButtonPrimary>
+          <Image source={studyIcon} />
+          <S.ButtonText>Estudar</S.ButtonText>
+        </S.ButtonPrimary>
+        <S.ButtonSecondary onPress={handleNavigateToGiveClassesPage}>
+          <Image source={giveClassesIcon} />
+          <S.ButtonText>Dar aulas</S.ButtonText>
+        </S.ButtonSecondary>
+      </S.ButtonsContainer>
+
+      <S.TotalConnections>
+        Total de 285 conexões já realizadas
+        <Image source={heartIcon} />
+      </S.TotalConnections>
+    </S.LandingPageWrapper>
+  )
+}
 
 export default Landing
