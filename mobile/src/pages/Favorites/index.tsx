f6a23b39c5e97fc7c6ee4c20a97d947dc281c29a
Felipe Suri <felipeheredia1234@gmail.com>
2020-08-07 10:08:32 -0300

    :sparkles: use PageHeader

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
+import PageHeader from '../../components/PageHeader'
+
 import * as S from './styled'
 
 const Favorites: React.FC = () => {
-  return <S.FavoritesWrapper />
+  return (
+    <S.FavoritesWrapper>
+      <PageHeader title="Seus Proffys favoritos" />
+    </S.FavoritesWrapper>
+  )
 }
 
 export default Favorites
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 import * as S from './styled'
+import PageHeader from '../../components/PageHeader'
 
 const TeacherList: React.FC = () => {
-  return <S.TeacherListWrapper />
+  return (
+    <S.TeacherListWrapper>
+      <PageHeader title="Proffys disponíveis" />
+    </S.TeacherListWrapper>
+  )
 }
 
 export default TeacherList
