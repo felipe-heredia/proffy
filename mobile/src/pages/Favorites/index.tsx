63e02c6c262816394389237eab81b20cfc443071
Felipe Suri <felipeheredia1234@gmail.com>
2020-08-07 09:03:26 -0300

    :zap: create Favorites page

---
+++ mobile/src/pages/Favorites/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import * as S from './styled'
+
+const Favorites: React.FC = () => {
+  return <S.FavoritesWrapper />
+}
+
+export default Favorites
---
+++ mobile/src/pages/Favorites/styled.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native'
+
+export const FavoritesWrapper = styled.View`
+  flex: 1;
+  background: #f0f0f7;
+`
