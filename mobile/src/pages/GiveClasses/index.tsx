02e92f978d882f24384ab6b625a4a1009d71285f
Felipe Suri <felipeheredia1234@gmail.com>
2020-08-07 08:31:50 -0300

    :sparkles: style GiveClasses page

--- mobile/src/pages/GiveClasses/index.tsx
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -1,12 +1,31 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+
+import giveClassesBgImage from '../../assets/images/give-classes-background.png'
 
 import * as S from './styled'
 
-const GiveClasses: React.FC = () => (
-  <S.GiveClassesWrapper>
-    <Text>Hello</Text>
-  </S.GiveClassesWrapper>
-)
+const GiveClasses: React.FC = () => {
+  const { goBack } = useNavigation()
+
+  function handleNavigateBack() {
+    goBack()
+  }
+
+  return (
+    <S.GiveClassesWrapper>
+      <S.ImageContent resizeMode="contain" source={giveClassesBgImage}>
+        <S.Title>Quer ser um Proffy?</S.Title>
+        <S.Description>
+          Para começar você precisa se cadastrar como professor na nossa plataforma web
+        </S.Description>
+      </S.ImageContent>
+
+      <S.Button onPress={handleNavigateBack}>
+        <S.ButtonText>Tudo bem!</S.ButtonText>
+      </S.Button>
+    </S.GiveClassesWrapper>
+  )
+}
 
 export default GiveClasses
